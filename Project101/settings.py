2d962012f545ded530d86e43ad3ab8461243e24c
SajalJha <sajaljhabtechcse22-26@liet.in>
2025-02-23 19:38:08 +0530

    Project 101

--- Project101/settings.py
+++ Project101/settings.py
@@ -16,9 +16,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-38axrzh@n80vyv^o#2nfbhpx-=d7%jlq1v(gban69ygp4m5n0-'
 
