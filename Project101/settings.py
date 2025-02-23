90950789052ad25764a05286bc5678faf8ae22c5
SajalJha <sajaljhabtechcse22-26@liet.in>
2025-02-23 19:49:06 +0530

    setting.py changes

--- Project101/settings.py
+++ Project101/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-38axrzh@n80vyv^o#2nfbhpx-=d7%jlq1v(gban69ygp4m5n0-'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
